488b0dc0de3f03879c365219e8d2135369a7533a
simon olofsson <telsak@hotmail.se>
2023-12-12 00:55:59 +0100

    Day 5 - Init

---
+++ 2023/day5.py
@@ -0,0 +1,47 @@
+'''
+   ╒══════════════════╕ ╓─ ──── ─ ──══── ─ ────── ─ ────── ─ ──══── ─ ──── ─┐ 
+ ┌─┤▌  SCRIPT INFO   ▐├─╜ ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀ └┐
+ █ ╘══════════════════╛                                                      █
+ █   [ Filename ................................................ day5.py ]   █
+ █   [ Type ................................. Advent of Code 2022 puzzle ]   █
+ █                                                                           █
+ █             [ Written by ........................... telsak ]             █
+ █             [ Created date ................... Dec 11, 2023 ]             █
+ └┐ ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄ ┌┘
+  └ ──── ─ ──══── ─ ────── ─ ───── ─ ──══── ─ ──── ─ ────── ─ ──══── ─ ──── ┘
+'''
+
+import time
+import sys
+
+def get_data(ns=0):
+    day_file = sys.argv[0].split('.')[0]
+    with open (f'.//input//{day_file}_input{ns * "_small"}', 'r') as file:
+        filedata = [line.strip() for line in file.readlines()]
+    return filedata
+    
+def part_one():
+    # do stuff
+    return
+
+def part_two():
+    # do stuff again
+    return
+
+full_or_not = '--full' in sys.argv
+data = get_data(small_or_not)
+
+# part one or two? part one is default
+if '-p2' in sys.argv:
+    print('== Running part two ==')
+    start = time.time()
+    output = part_two(data)
+else:
+    print('== Running part one (use -p2 for part two) ==')
+    start = time.time()
+    output = part_one(data)
+
+stop = time.time()
+
+print(f'Körning tog: {stop - start:.4f} sekunder\n{output}')
+
