c211b5b8b30056b753805cf75ed597e15aabfecd
simon olofsson <telsak@hotmail.se>
2023-12-11 23:41:59 +0100

    Day 3 - Part Two solved

--- 2023/day3.py
+++ 2023/day3.py
@@ -22,14 +22,23 @@ def get_data(ns=0):
     filedata.append('.' * len(filedata[1]))
     return filedata
 
-def get_box_data(x, y, length, schematic):
+def get_box_data(x, y, length, schematic, gear=0):
     # returns True if there is a valid symbol in a box around the number
     data = ''
     data += schematic[y-1][x-1:x+length+1]
     data += schematic[y][x-1:x+length+1]
     data += schematic[y+1][x-1:x+length+1]
-    newdata = [x for x in data if not x.isdigit() and x != '.']
-    return len(newdata) > 0
+    new_data = [_ for _ in data if not _.isdigit() and _ != '.']
+    if gear:
+        if '*' in data:
+            pos = data.find('*')
+            x = pos % (length+2) + x
+            y = pos // (length+2) + y
+            return True, (x, y)
+        else:
+            return False, (0, 0)
+    else:
+        return len(new_data) > 0
 
 def part_one(schematic):
     # do stuff
@@ -50,14 +59,38 @@ def part_one(schematic):
                     number = ''
     return parts_sum
 
-def part_two():
+def part_two(schematic):
     # do stuff again
-    return
+    ratios_sum = 0
+    gears = {}
+    number = ''
+    for y, line in enumerate(schematic):
+        for x, pos in enumerate(line):
+            if pos.isdigit():
+                if number == '':
+                    sy = y
+                    sx = x
+                number += pos
+            else:
+                if number != '':
+                    near_gear, coords = get_box_data(sx, sy, len(number), schematic, 1)
+                    if near_gear:
+                        if coords in gears:
+                            gears[coords] += [number]
+                        else:
+                            gears[coords] = [number]
+                    number = ''
+    for coord, parts in gears.items():
+        if len(parts) == 2:
+            ratios_sum += (int(parts[0]) * int(parts[1]))
+
+    return ratios_sum
 
 start = time.time()
 
 data = get_data()
-output = part_one(data)
+#output = part_one(data)
+output = part_two(data)
 
 stop = time.time()
 
