9702661e448830fb72769c9d53852c36de1e5cc5
simon olofsson <telsak@hotmail.se>
2023-12-12 00:47:17 +0100

    Day 4 - Part Two solved

--- 2023/day4.py
+++ 2023/day4.py
@@ -25,17 +25,26 @@ def check_card(card):
     winners, mine = card.split('|')
     return id.split()[1], winners.split(), mine.split()
 
-def tally_points(winning_numbers, drawn_numbers):
+def tally_points(winning_numbers, drawn_numbers, points=1):
     points = 0
     luck = [n for n in drawn_numbers if n in winning_numbers]
-    if len(luck) > 0:
-        for n in luck:
-            if points == 0:
-                points += 1
-            else:
-                points *= 2
-    return points
+    if points:
+        if len(luck) > 0:
+            for n in luck:
+                if points == 0:
+                    points += 1
+                else:
+                    points *= 2
+        return points
+    else:
+        return len(luck)
 
+def look_ahead(id, cards):
+    number_of_cards = 1
+    for i in range(id+1, id+1+cards[id]['wins']):
+        if i <= len(cards):
+            number_of_cards += look_ahead(i, cards)
+    return number_of_cards
 
 def part_one(indata):
     # do stuff
@@ -45,15 +54,23 @@ def part_one(indata):
         won_points += tally_points(winners, mine)
     return won_points
 
-def part_two():
+def part_two(indata):
     # do stuff again
-    return
+    total_scratch_cards = 0
+    cards = {}
+    for card in indata:
+        id, winners, drawn = check_card(card)
+        wins = tally_points(winners, drawn, 0)
+        cards[int(id)] = {'winners': winners, 'drawn': drawn, 'wins': int(wins)}
+    for recorded_card, _ in cards.items():
+        total_scratch_cards += look_ahead(recorded_card, cards)
+    return total_scratch_cards
 
 start = time.time()
 
 data = get_data()
-output = part_one(data)
-#output = part_two(data)
+#output = part_one(data)
+output = part_two(data)
 
 stop = time.time()
 
