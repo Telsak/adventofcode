e1aa2ecd7bf0ff9cc8b5e8690675a9582053dcfd
simon olofsson <telsak@hotmail.se>
2023-12-11 22:34:45 +0100

    Day 3 - Part One solved

--- 2023/day3.py
+++ 2023/day3.py
@@ -17,12 +17,38 @@ import sys
 def get_data(ns=0):
     day_file = sys.argv[0].split('.')[0]
     with open (f'.//input//{day_file}_input{ns * "_small"}', 'r') as file:
-        filedata = [line.strip() for line in file.readlines()]
+        filedata = ['.' + line.strip() + '.' for line in file.readlines()]
+    filedata.insert(0, '.' * len(filedata[1]))
+    filedata.append('.' * len(filedata[1]))
     return filedata
-    
-def part_one():
+
+def get_box_data(x, y, length, schematic):
+    # returns True if there is a valid symbol in a box around the number
+    data = ''
+    data += schematic[y-1][x-1:x+length+1]
+    data += schematic[y][x-1:x+length+1]
+    data += schematic[y+1][x-1:x+length+1]
+    newdata = [x for x in data if not x.isdigit() and x != '.']
+    return len(newdata) > 0
+
+def part_one(schematic):
     # do stuff
-    return
+    parts_sum = 0
+    sx, sy = 0, 0
+    number = ''
+
+    for y, line in enumerate(schematic):
+        for x, pos in enumerate(line):
+            if pos.isdigit():
+                if number == '':
+                    sy = y
+                    sx = x
+                number += pos
+            else:
+                if number != '':
+                    parts_sum += get_box_data(sx, sy, len(number), schematic) * int(number)
+                    number = ''
+    return parts_sum
 
 def part_two():
     # do stuff again
@@ -30,7 +56,7 @@ def part_two():
 
 start = time.time()
 
-data = get_data(True)
+data = get_data()
 output = part_one(data)
 
 stop = time.time()
