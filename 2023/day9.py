7ef5374f68dd0c0e0fca8dd64b0d6cc5c90268aa
simon olofsson <telsak@hotmail.se>
2023-12-15 09:20:57 +0100

    Day 9 - Part Two solved

--- 2023/day9.py
+++ 2023/day9.py
@@ -25,21 +25,30 @@ def expand_history(h):
     # and do this for every subsequent line
     return [h[i+1]-h[i] for i in range(len(h)-1)]
 
-def predict_history(h):
+def predict_future(h):
     for i in range(len(h)-1, -1, -1):
         if i > 0:
             h[i-1][-1] = h[i][-1] + h[i-1][-2]
     return h
 
-    
+def predict_past(h):
+    for i in range(len(h)-1, -1, -1):
+        if i > 0:
+            h[i-1][0] = h[i-1][1] - h[i][0]
+    return h
+
 def display_grid(h):
-    h = predict_history(h)
+    if '-p2' in sys.argv:
+        h = predict_past(h)
+        rval = h[0][0]
+    else:
+        h = predict_future(h)
+        rval = h[0][-1]
     print(250 * '=')
     for line in h:
         print(f"{''.join(str(value).center(10) for value in line): ^250}")
-    print('==== value out:', h[0][-1], '====================')
-    return h[0][-1]
-
+    print('==== value out:', rval, '====================')
+    return rval
 
 def part_one(indata):
     # do stuff
@@ -57,13 +66,27 @@ def part_one(indata):
 
     for history in histories:
         sum_values += display_grid(history)
-    
 
     return sum_values
 
 def part_two(indata):
     # do stuff again
-    return
+    histories = []
+    sum_values = 0
+    for line in indata:
+        history = [[int(x) for x in line.split()]]
+        while not all(value == 0 for value in history[-1]):
+            history.append(expand_history(history[-1]))
+        histories.append(history)
+
+    for history in histories:
+        for line in history:
+            line.insert(0, 0)
+
+    for history in histories:
+        sum_values += display_grid(history)
+
+    return sum_values
 
 full_or_not = '--full' not in sys.argv
 data = get_data(full_or_not)
