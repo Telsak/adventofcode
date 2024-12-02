ff38a09584aa7b78ca2780a84c8b484c3213f9e2
simon olofsson <telsak@hotmail.se>
2024-12-02 15:46:52 +0100

    2024: Day 2 - Part 2

--- 2024/day02.py
+++ 2024/day02.py
@@ -52,7 +52,53 @@ def part_one(indata):
 
 def part_two(indata):
   # do stuff again
-  return
+  unsafe = []
+  safe_reports = 0
+  for level in indata:
+    level = [int(x) for x in level.split()]
+    # only accept reports that are increasing or decreasing
+    if level == sorted(level) or level == sorted(level, reverse=True):
+      safe = check_delta(level, 0, 4)
+    else:
+      safe = False
+    # try to get the report safe
+    if not safe:
+      unsafe.append(level)
+
+    safe_reports += safe
+
+  for entry in unsafe:
+    safe_reports += problem_dampener(entry)
+  return safe_reports
+
+def check_delta(data, min_v, max_v):
+  # determine if the submitted list passes the safe parameters
+  safe = True
+  for i in range(len(data)-1):
+    if safe:
+      delta = abs(data[i] - data[i+1])
+      # if the delta is within safe parameters
+      if delta > min_v and delta < max_v:
+        continue
+      else:
+        safe = False
+  return safe
+  
+def problem_dampener(indata, n=0, reason=''):
+  # I will go out of bounds here..
+  if n == len(indata):
+    return False
+  report = indata.copy()
+  report.pop(n)
+  if report == sorted(report) or report == sorted(report, reverse=True):
+    safe = check_delta(report, 0, 4)
+  else:
+    safe = False
+
+  if safe == False:
+    n += 1
+    safe = problem_dampener(indata, n, reason)
+  return safe
 
 full_or_not = '--full' not in sys.argv
 data = get_data(full_or_not)
