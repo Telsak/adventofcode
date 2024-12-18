16bf3d09d7f5730ad656f16f5d2f926815fab548
simon olofsson <telsak@hotmail.se>
2024-12-18 12:30:33 +0100

    2024: Day 9 - Part 2

--- 2024/day09.py
+++ 2024/day09.py
@@ -30,10 +30,11 @@ def calc_checksum(_disk):
   for position, id in enumerate(_disk):
     # print(f'{position} * {id} = {position * int(id)}')
     # print(f'{position}/{x}\r')
-    chk += position * int(id)
+    if id != '.':
+      chk += position * int(id)
   return chk
 
-def generate_diskmap(diskdata):
+def generate_diskmap(diskdata, part=1):
   # generate the map of where disk files are saved
   disk = []
   pos = 0
@@ -46,13 +47,12 @@ def generate_diskmap(diskdata):
     else:
       # add empty spots as many times as there are blocks indicated in [i]
       epos = int(diskdata[i])
-      disk += ['.' for x in range(epos)]
+      disk += ['.' for _ in range(epos)]
   return disk
 
 def part_one(indata):
-  
   disk = generate_diskmap(indata)
-
+  input(disk)
   # as long as there are free spaces, shuffle elements from the back
   di = 0
   try:
@@ -71,8 +71,6 @@ def part_one(indata):
       else:
         while disk[-1] == '.':
           disk.pop()
-
-      #input(disk)
   except:
     print('index out of bound!')
 
@@ -80,10 +78,52 @@ def part_one(indata):
   return checksum
 
 def part_two(indata):
-  # do stuff again
-  return
+  disk = generate_diskmap(indata, part=2)
+  # a lot of previous attempts were with using .pop() to pull things from the
+  # end of the list, but that is counterproductive now when some elements
+  # can and should be left where they are if they dont fit
+  # we still work from right to left
+  
+  # set the seen id to -1 to begin with, and keep track of the number of times
+  # where the id has been seen. When something new is seen, either a space or
+  # a new digit, save the index+1 (which is the previous position from right)
+  # and now you know how many positions the file takes up
 
+  seen_id = -1
 
+  for r_pos, file_id in reversed(list(enumerate(disk))):
+    if file_id != seen_id and file_id != '.':
+      i = r_pos
+      id_count = 1
+      fl = [i]
+      seen_id = file_id
+      # now we know how many there are of 
+      while i > 0 and disk[i-1] == file_id:
+        id_count += 1
+        i -= 1
+        fl += [i]
+      # look for empty spaces up to r_pos index
+      sl = [x for x in fl]; lsl = len(sl)
+      s_count = 0
+      for l_pos in range(r_pos):
+        # if this isn't a free space, ignore it and move to the next
+        if disk[l_pos] != '.':
+          s_count = 0
+          continue
+        else:
+          # set up to start tracking spaces
+          s_count += 1
+          if s_count == 1:
+            l_start = l_pos
+          if s_count == lsl:
+            l_end = l_pos
+            for s in range(l_start, l_end + 1):
+              disk[s], disk[sl[s - l_start]] = disk[sl[s - l_start]], disk[s]
+            break
+      continue
+    else:
+      continue
+  return calc_checksum(disk)
 
 full_or_not = '--full' not in sys.argv
 data = get_data_f(full_or_not, 'lines')
@@ -92,7 +132,7 @@ data = get_data_f(full_or_not, 'lines')
 if '-p2' in sys.argv:
   print('== Running part two ==')
   start = time.time()
-  output = part_two(data)
+  output = part_two(data[0])
 else:
   print('== Running part one (use -p2 for part two) ==')
   start = time.time()
