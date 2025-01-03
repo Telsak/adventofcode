fdc48b039836ee68aa094b8746746a0af46d0745
simon olofsson <telsak@hotmail.se>
2025-01-03 14:08:18 +0100

    Utils update

--- 2024/aoc_utils.py
+++ 2024/aoc_utils.py
@@ -22,5 +22,17 @@ def get_data_f(ns=0, mode='lines'):
       filedata = [line.strip() for line in file.readlines()]
   return filedata
 
-
+colors = [
+    "\033[38;2;0;0;128m",     # Dark Blue
+    "\033[38;2;0;32;160m",    # Deep Blue
+    "\033[38;2;0;64;192m",    # Medium Blue
+    "\033[38;2;0;128;255m",   # Light Blue
+    "\033[38;2;64;192;255m",  # Cyan
+    "\033[38;2;128;255;192m", # Aqua Green
+    "\033[38;2;128;255;128m", # Light Green
+    "\033[38;2;192;255;64m",  # Lime Green
+    "\033[38;2;255;192;0m",   # Golden Yellow
+    "\033[38;2;255;0;0m",     # Red
+    "\033[0m"                 # reset
+]
 
