9c730d128f736caddca9088280c03a6cfc54cb93
simon olofsson <telsak@hotmail.se>
2025-12-04 07:19:48 +0100

    aoc_utils update and day 4 blank file

---
+++ 2025/day04.py
@@ -0,0 +1,45 @@
+'''
+   ╒══════════════════╕ ╓─ ──── ─ ──══── ─ ────── ─ ────── ─ ──══── ─ ──── ─┐ 
+ ┌─┤▌  SCRIPT INFO   ▐├─╜ ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀ └┐
+ █ ╘══════════════════╛                                                      █
+ █   [ Filename ............................................... day04.py ]   █
+ █   [ Type ................................. Advent of Code 2025 puzzle ]   █
+ █                                                                           █
+ █   Day 4: Printing Department                                              █
+ █   https://adventofcode.com/2025/day/4                                     █
+ █                                                                           █
+ █             [ Written by ........................... telsak ]             █
+ █             [ Created date ................... Dec 04, 2025 ]             █
+ └┐ ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄ ┌┘
+  └ ──── ─ ──══── ─ ────── ─ ───── ─ ──══── ─ ──── ─ ────── ─ ──══── ─ ──── ┘
+'''
+
+from aoc_utils import get_data
+import time
+import sys
+
+def part_one(indata):
+  # do stuff
+  return indata
+
+def part_two(indata):
+  # do stuff again
+  return
+
+full_or_not = '--full' not in sys.argv
+data = get_data(full_or_not)
+
+# part one or two? part one is default
+if '-p2' in sys.argv:
+  print('== Running part two ==')
+  start = time.time()
+  output = part_two(data)
+else:
+  print('== Running part one (use -p2 for part two) ==')
+  start = time.time()
+  output = part_one(data)
+
+stop = time.time()
+
+print(f'Runtime was: {stop - start:.4f} seconds\n{output}')
+
