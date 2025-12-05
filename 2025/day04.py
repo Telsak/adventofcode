d944a789b0423ba96467198f5a109ea228a92ce0
simon olofsson <telsak@hotmail.se>
2025-12-05 10:12:30 +0100

    2025: Day 4 - Part 2

--- 2025/day04.py
+++ 2025/day04.py
@@ -24,7 +24,10 @@ def sweep(x, y, data, targ):
   return lookup.count(targ)
 
 def part_one(indata):
-  # do stuff
+  # simple, the indata is a 2d grid, we add a buffer border around it and just
+  # run a concatenation of the cells and count how many times the paper roll
+  # occurs in it. We use 5 in the comparison as the roll on the [r][c] coord
+  # gets included in the source data
   indata = add_border_to_2d_grid(indata, '.')
 
   rows = len(indata)
@@ -40,8 +43,35 @@ def part_one(indata):
   return rolls
 
 def part_two(indata):
-  # do stuff again
-  return
+  # feels like there's a trap here if the list is modified immediately to
+  # remove the roll which 'desyncs' the lookups. Just drop found rolls in a
+  # coordinate list and after each pass we march the list and swap those fields
+  # in the source data
+
+  indata = add_border_to_2d_grid(indata, '.')
+
+  rows = len(indata)
+  cols = len(indata[0])
+  rolls = 0
+  symb = '@'
+  
+  all_rolls_removed = False
+  rolls_to_remove = []
+
+  while all_rolls_removed is False:
+    for r in range(1, rows-1):
+      for c in range(1, cols-1):
+        if indata[r][c] == symb and sweep(c, r, indata, symb) < 5:
+          rolls += 1
+          rolls_to_remove.append((r, c))
+
+    if len(rolls_to_remove) > 0:
+      while rolls_to_remove:
+        r, c = rolls_to_remove.pop()
+        indata[r][c] = '.'
+    else:
+      all_rolls_removed = True
+  return rolls
 
 full_or_not = '--full' not in sys.argv
 data = get_data(full_or_not, 'grid')
