29eb3ec313bedb8a171418dbcbd159d01f62bd9c
simon olofsson <telsak@hotmail.se>
2025-12-15 17:13:50 +0100

    2025: Day 6 - Part 2

--- 2025/day06.py
+++ 2025/day06.py
@@ -19,6 +19,21 @@ from aoc_utils import get_data, get_table_column
 import time
 import sys
 from math import prod
+import re
+
+def parse_table_column(table, col_index):
+  # based on the str data in table[i][ci], use the operator in table[-2][ci]
+  # to determine how to add the number
+  _lines = get_table_column(table[:-2], col_index)
+  lines = []
+  total = 0
+  for i in range(len(_lines[0])):
+    _line = ''.join(get_table_column(_lines, i))
+    _line = _line.replace('0', '')
+    _line = int(_line)
+    lines.append(_line)
+
+  return lines
 
 def part_one(indata):
   # convert the indata lines to a grid table, split on spaces
@@ -38,19 +53,65 @@ def part_one(indata):
   return total
 
 def part_two(indata):
-  # do stuff again
-  return
+  # Lets start the same as p1, but then process the column line again
+  # what if we for each line find the longest value and pad existing fields
+  # with zeroes based on that length?
+  # doesn't work as some columns have empty space before and some after
+  # the actual numbers
+  indata = indata.split('\n')
+  indata.remove('')
+  
+  #bottom row is size of column
+  indata.append([0 for x in indata[0].split()])
+  for line in indata[:-1]:
+    for i, col in enumerate(line.split()):
+      if len(col) > indata[-1][i]:
+        indata[-1][i] = len(col)
+
+  table = []
+  for line in indata[:-2]:
+    _line = ''
+    # work in small batches and just manually set spaces to 0 if they
+    # fall within the column area, based on the size already parsed from
+    # the first pass
+    x = 0
+    for i, seq in enumerate(indata[-1]):
+      for j in range(seq):
+        if line[x] == ' ':
+          _line += '0'
+        else:
+          _line += line[x]
+        x += 1
+      _line += ' '
+      x += 1
+    table.append(_line.rstrip().split())
+  table.append(indata[-2].split())
+  table.append(indata[-1])
+
+  # at this point I have a table with some data that also has the size of
+  # all columns at the lowest row, now just send that to a modified parse
+  # function that calls the table column function from part 1 and voila  
+  total = 0
+  for i in range(len(table[-1])):
+    values = parse_table_column(table, i)
+    if table[-2][i] == '*':
+      total += prod(values)
+    else:
+      total += sum(values)
+  
+  return total
 
 full_or_not = '--full' not in sys.argv
-data = get_data(full_or_not)
 
 # part one or two? part one is default
 if '-p2' in sys.argv:
   print('== Running part two ==')
+  data = get_data(full_or_not, 'raw')
   start = time.time()
   output = part_two(data)
 else:
   print('== Running part one (use -p2 for part two) ==')
+  data = get_data(full_or_not)
   start = time.time()
   output = part_one(data)
 
